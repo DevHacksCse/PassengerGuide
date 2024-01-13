d0112480bccccf4ac09b2b4f02eca387eb16fd8e
Om Kokate <om.kokate2902@gmail.com>
2024-01-13 15:18:06 +0530

    lost and found #1

---
+++ app/src/main/java/com/hackstudio/passengerguide/LostAndFound/ReportLostItemActivity.java
@@ -0,0 +1,16 @@
+package com.hackstudio.passengerguide.LostAndFound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackstudio.passengerguide.R;
+
+public class ReportLostItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_lost_item);
+    }
+}
